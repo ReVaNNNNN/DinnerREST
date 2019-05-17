76eaaf4468c41959259f7b3b0d2f6d0a9e8c896b
ReVaNNNNN <revannn5480@gmail.com>
2019-05-17 08:25:53 +0200

    Test commit.

---
+++ tests/Models/ComponentTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: revan
+ * Date: 01.03.2019
+ * Time: 07:13
+ */
+
+namespace Tests\Models;
+
+
+use Tests\TestCase;
+
+class ComponentTest extends TestCase
+{
+
+}
